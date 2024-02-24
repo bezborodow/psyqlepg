769fc16aa007aed5f30d23c273d2a050d7f3b2d2
Damien Bezborodov <dbezborodov@gmail.com>
2024-02-25 05:50:58 +1030

    Column info.

--- src/psyqlepg/table_info.py
+++ src/psyqlepg/table_info.py
@@ -9,8 +9,6 @@ class TableInfo:
     def unique_indices(self, conn):
         '''
         Search for unique keys (except for primary keys.)
-
-        https://www.postgresql.org/docs/current/functions-info.html
         '''
 
         cur = conn.cursor()
@@ -18,6 +16,8 @@ class TableInfo:
 
         # The indexdef is an array of column names on the unique index.
         # The relname is the name of the unique index.
+        #
+        # https://www.postgresql.org/docs/current/functions-info.html
         query = '''
             select idx.relname,
                     json_agg(pg_catalog.pg_get_indexdef(a.attrelid, a.attnum, true)) indexdef
@@ -43,3 +43,40 @@ class TableInfo:
 
         return indices
 
+
+    def primary_key(self, conn):
+        '''
+        Retreive the primary key. This is either a string, or a tuple
+        if the primary key is a composite key.
+        '''
+
+        # https://wiki.postgresql.org/wiki/Retrieve_primary_key_columns
+        query = '''
+            SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS data_type
+            FROM   pg_index i
+            JOIN   pg_attribute a ON a.attrelid = i.indrelid
+                    AND a.attnum = ANY(i.indkey)
+            WHERE  i.indrelid = 'tablename'::regclass
+            AND    i.indisprimary;
+        '''
+		return NotImplemented # TODO
+
+
+    def columns(self, conn):
+        cur = conn.cursor()
+        cur.row_factory = namedtuple_row
+        query = '''
+            select data_type, column_default, udt_name, column_name,
+                    character_maximum_length, numeric_precision,
+                    is_nullable = 'YES' as is_nullable,
+                    is_self_referencing = 'YES' as is_self_referencing,
+                    is_updatable = 'YES' as is_updatable
+            from information_schema.columns
+            where table_name = %s
+                and table_schema = 'public';
+        '''
+        cur.execute(query, [self.table.table])
+        columns = {}
+        while column := cur.fetchone():
+            columns[column.column_name] = column
+        return columns
